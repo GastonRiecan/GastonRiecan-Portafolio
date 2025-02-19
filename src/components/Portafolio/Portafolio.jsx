5a8cd3064df12069fad45595d0253ee5bba296f1
Gaston_Riecan <g.e.riecan@gmail.com>
2025-02-19 19:57:21 -0300

    Portfolio updated

--- src/components/Portafolio/Portafolio.jsx
+++ src/components/Portafolio/Portafolio.jsx
@@ -5,9 +5,42 @@ const Portafolio = () => {
     <div className="portafolio">
       <h1 className="h1-portafolio">PORTAFOLIO</h1>
       <div className="proyectos">
-        <div className="proyecto-desplegado1">proyecto-1</div>
-        <div className="proyecto-desplegado2">proyecto-2</div>
-        <div className="proyecto-desplegado3">proyecto-3</div>
+        <div className="proyecto-desplegado1">
+          <h3>Clon de Slack</h3>
+          <img src="https://github.com/GastonRiecan/images-repository/raw/main/images/images.png" alt="Slack portada" />
+          <a href="https://slack-utn.vercel.app/">
+            <i className="bi bi-link-45deg">&nbsp;Ingresar</i>
+          </a>
+          <span>
+            <a href="https://github.com/GastonRiecan/Slack_UTN">
+              <i className="bi bi-github">&nbsp;Mira como esta hecho</i>
+            </a>
+          </span>
+        </div>
+        <div className="proyecto-desplegado2">
+        <h3>Aplicacion de Notas</h3>
+          <img src="https://github.com/GastonRiecan/images-repository/raw/main/images/notas.jpg" alt="Apps Notes portada" />
+          <a href="https://slack-utn.vercel.app/">
+            <i className="bi bi-link-45deg">&nbsp;Ingresar</i>
+          </a>
+          <span>
+            <a href="https://notes-app-psi-ashen.vercel.app/">
+              <i className="bi bi-github">&nbsp;Mira como esta hecho</i>
+            </a>
+          </span>
+        </div>
+        <div className="proyecto-desplegado3">
+        <h3>Calculadora con Javascript</h3>
+          <img src="https://github.com/GastonRiecan/images-repository/raw/main/images/calculadora-cientifica.jpg" alt="Calculadora Image" />
+          <a href="https://slack-utn.vercel.app/">
+            <i className="bi bi-link-45deg">&nbsp;Ingresar</i>
+          </a>
+          <span>
+            <a href="https://notes-app-psi-ashen.vercel.app/">
+              <i className="bi bi-github">&nbsp;Mira como esta hecho</i>
+            </a>
+          </span>
+        </div>
       </div>
       <hr />
     </div>
