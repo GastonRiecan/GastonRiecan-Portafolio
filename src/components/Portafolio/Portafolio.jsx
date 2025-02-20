ae40a084d8a90905032e0223ae9860dd3b2db140
Gaston_Riecan <g.e.riecan@gmail.com>
2025-02-20 20:16:10 -0300

    links to proyects updated

--- src/components/Portafolio/Portafolio.jsx
+++ src/components/Portafolio/Portafolio.jsx
@@ -8,11 +8,15 @@ const Portafolio = () => {
         <div className="proyecto-desplegado1">
           <h3>Clon de Slack</h3>
           <img src="https://github.com/GastonRiecan/images-repository/raw/main/images/images.png" alt="Slack portada" />
-          <a href="https://slack-utn.vercel.app/">
+          <a 
+          target="_blank"
+          href="https://slack-utn.vercel.app/">
             <i className="bi bi-link-45deg">&nbsp;Ingresar</i>
           </a>
           <span>
-            <a href="https://github.com/GastonRiecan/Slack_UTN">
+            <a 
+            target="_blank"
+            href="https://github.com/GastonRiecan/Slack_UTN">
               <i className="bi bi-github">&nbsp;Mira como esta hecho</i>
             </a>
           </span>
@@ -20,7 +24,9 @@ const Portafolio = () => {
         <div className="proyecto-desplegado2">
         <h3>Aplicacion de Notas</h3>
           <img src="https://github.com/GastonRiecan/images-repository/raw/main/images/notas.jpg" alt="Apps Notes portada" />
-          <a href="https://slack-utn.vercel.app/">
+          <a 
+          target="_blank"
+          href="https://notes-app-psi-ashen.vercel.app/">
             <i className="bi bi-link-45deg">&nbsp;Ingresar</i>
           </a>
           <span>
@@ -32,11 +38,11 @@ const Portafolio = () => {
         <div className="proyecto-desplegado3">
         <h3>Calculadora con Javascript</h3>
           <img src="https://github.com/GastonRiecan/images-repository/raw/main/images/calculadora-cientifica.jpg" alt="Calculadora Image" />
-          <a href="https://slack-utn.vercel.app/">
+          <a href="">
             <i className="bi bi-link-45deg">&nbsp;Ingresar</i>
           </a>
           <span>
-            <a href="https://notes-app-psi-ashen.vercel.app/">
+            <a href="">
               <i className="bi bi-github">&nbsp;Mira como esta hecho</i>
             </a>
           </span>
