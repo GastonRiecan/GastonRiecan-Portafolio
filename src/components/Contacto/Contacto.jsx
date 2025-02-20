b0e28241b42692948b3c40611a7c1f40778db0cc
Gaston_Riecan <g.e.riecan@gmail.com>
2025-02-20 20:36:31 -0300

    contact links updated

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -7,15 +7,25 @@ const Contacto = () => {
       <div className="contacto">
         <span>
           <i className="bi bi-envelope-at-fill"></i>
-          &nbsp;&nbsp;Email: g.e.riecan@gmail.com
+          &nbsp;Email:
+          <a href="mailto:g.e.riecan@gmail.com">
+          &nbsp;&nbsp;g.e.riecan@gmail.com
+        </a>
         </span>
         <span>
           <i className="bi bi-whatsapp"></i>
-          &nbsp;&nbsp;Whatsapp: +54 11 62575808
+          &nbsp;&nbsp;Whatsapp:
+          <a
+            href="https://wa.me/+541162575808"
+            target="_blank"
+            rel="noopener noreferrer">&nbsp;+54 1162575808</a>
         </span>
         <span>
           <i className="bi bi-linkedin"></i>
-          &nbsp;&nbsp;Linkedin: https://www.linkedin.com/in/gastonriecan/
+          &nbsp;&nbsp;Linkedin:
+          <a 
+          target="_blank"
+          href="https://www.linkedin.com/in/gastonriecan/">&nbsp;https://www.linkedin.com/in/gastonriecan/</a>
         </span>
         <h3>ESTE SITIO ESTA DESARROLLADO CON:</h3>
         <div className="icons-dev">
