1ee0efdeadec36ab162d9abae3c51f97c8c0f988
Gaston_Riecan <g.e.riecan@gmail.com>
2025-02-22 13:48:21 -0300

    css updates

--- src/components/Educacion/Educacion.jsx
+++ src/components/Educacion/Educacion.jsx
@@ -10,11 +10,11 @@ const Educacion = () => {
   return (
     <div className="educacion">
       <h1 className="h1-educacion">EDUCACION</h1>
-      <div className="educacion-utn">
-        <h2 className="h2-educacion">
-          <span>DIPLOMATURA FULLSTACK JAVASCRIPT</span>
+      <div className="edu-container">
+        <div className="head-edu">
+          <h2>DIPLOMATURA FULLSTACK JAVASCRIPT</h2>
           <span className="fecha">Abril 2024-Diciembre 2024</span>
-        </h2>
+        </div>
         <h3>
           Diplomatura con aprendizaje de React, Node.js, Express, MongoDB y
           Mysql.
@@ -27,11 +27,11 @@ const Educacion = () => {
         onClick={()=>verCertificado("https://drive.google.com/file/d/1pSz6XeQU19Pt93L7puf08RuXkltikFV8/view?usp=drive_link")}
         className="ver-certificado">Ver certificado</button>
       </div>
-      <div className="educacion-universitas">
-        <h2 className="h2-educacion">
-          <span>TECNICATURA SUPERIOR EN PROGRAMACION</span>
+      <div className="edu-container">
+        <div className="head-edu">
+          <h2>TECNICATURA SUPERIOR EN PROGRAMACION</h2>
           <span className="fecha">Abril 2022-Diciembre 2024</span>
-        </h2>
+        </div>
         <h3>
           Tecnicatura universitaria, fundamentos de programación y arquitectura
           de sistemas.
@@ -43,11 +43,11 @@ const Educacion = () => {
         onClick={verCertificado}
         className="ver-certificado">Ver certificado</button>
       </div>
-      <div className="educacion-aws">
-        <h2 className="h2-educacion">
-          <span>CURSO AWS CLOUD COMPUTING</span>
+      <div className="edu-container">
+        <div className="head-edu">
+          <h2>CURSO AWS CLOUD COMPUTING</h2>
           <span className="fecha">Diciembre 2024 (en curso)</span>
-        </h2>
+        </div>
         <h3>
           Curso de AWS Cloud Computing con aprendizaje de AWS y manejo de
           servicios en la nube.
@@ -58,29 +58,29 @@ const Educacion = () => {
         </h3>
         <button className="ver-certificado">Ver certificado</button>
       </div>
-      <div className="educacion-java">
-        <h2 className="h2-educacion">
-          <span>PROGRAMACION ORIENTADA A OBJETOS EN JAVA</span>
+      <div className="edu-container">
+        <div className="head-edu">
+          <h2>PROGRAMACION ORIENTADA A OBJETOS EN JAVA</h2>
           <span className="fecha">Abril 2023-Octubre 2023</span>
-        </h2>
+        </div>
         <h3>Programacion orientada a objetos en Java (Nivel principiante).</h3>
         <h3>(Argentina Programa 4.0, modalidad remoto, BSAS Argentina.)</h3>
         <button className="ver-certificado">Ver certificado</button>
       </div>
-      <div className="educacion-arg-prog">
-        <h2 className="h2-educacion">
-          <span>INTRODUCCION A LA PROGRAMACION</span>
+      <div className="edu-container">
+        <div className="head-edu">
+          <h2>INTRODUCCION A LA PROGRAMACION</h2>
           <span className="fecha">Octubre 2022-Diciembre 2022</span>
-        </h2>
+        </div>
         <h3>Fundamentos de programación (Herramienta utilizada: Pseint).</h3>
         <h3>(Argentina Programa 4.0, modalidad remoto, BSAS Argentina.)</h3>
         <button className="ver-certificado">Ver certificado</button>
       </div>
-      <div className="educacion-solo-learn">
-        <h2 className="h2-educacion">
-          <span>CURSOS TERMINADOS EN SOLO-LEARN Y FREECODECAMP</span>
+      <div className="edu-container">
+        <div className="head-edu">
+          <h2>CURSOS TERMINADOS EN SOLO-LEARN Y FREECODECAMP</h2>
           <span className="fecha">Abril 2022</span>
-        </h2>
+        </div>
         <h3>
           Conocimientos en JavaScript, HTML, CSS, C, Python, Pascal, Django y
           React.
